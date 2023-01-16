e17018ac2151ceac02a91a778d77d50da8c942f6
sachinc <sachin@milenzer.com>
2023-01-17 02:14:20 +0530

    make edits to finalise and deploy to vercel

---
+++ README.md
@@ -0,0 +1,57 @@
+## Portfolio Website For Developers
+
+# Features
+
+- Responsive layout, works well on both Mobile and Desktop
+- Dark mode support. Click buttons to toggle dark mode and light mode.
+- Clear React components and tailwind styling.
+- Exact sections that a developer needs to showcase their skills.
+- Clear call to actions.
+
+# Pages
+
+I've kept the pages as simple as possible. Only the required sections are included with minimal content.
+
+## Landing
+
+## About
+
+## Projects
+
+## Experience
+
+## Contact
+
+<!-- ![Hompage](full-link/demo-images/contact.png) -->
+
+# Tech Stack
+
+The website is built in my favourite Tech Stack and deployed on [Vercel](https://vercel.com)
+
+- [Next.js](https://nextjs.org) for building React Components.
+- [tailwindcss](https://tailwindcss.com) for styling.
+- [React Rough Notation](https://roughnotation.com) for Hero section highlighting.
+- [Vercel](https://vercel.com) for deployments and CI/CD.
+
+# Usage
+
+- Clone the repository using `git clone `
+- Install all the modules by using `npm i` or `npm install` or `yarn`
+- Run the local development server by using `npm run dev` or `yarn dev`
+- Make the required edits and deploy to YOUR GitHub repo for CI/CD.
+
+# Deployment
+
+Deployment can be done in 3 easy steps.
+
+- Login to [Vercel](https://vercel.com) or signup for an account if you don't have one.
+- Create a `New Project` and select YOUR GitHub repository of the portfolio project.
+- Wait for Vercel to deploy your project to production.
+
+# License
+
+This template is completely open source and free to use. Use it for client projects or your own portfolio project.
+
+# Support
+
+<a href="" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a>
---
+++ constants/data.js
@@ -0,0 +1,69 @@
+const userData = {
+  githubUsername: "sachin1245",
+  name: "Sachin C",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/sachin.jpg",
+  email: "sachinwh@gmail.com",
+  phone: "+91 9880158343",
+  address: "Bangalore, India.",
+  projects: [
+    {
+      title: "Springboard",
+      link: "https://springboard.com",
+      imgUrl: "/springboard.png",
+    },
+    {
+      title: "Bukuwarung",
+      link: "https://bukuwarung.com",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://csachin.in",
+      imgUrl: "/portfolio-dark.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `Experienced Frontend Developer with a demonstrated history of working in the e-learning industry. Skilled in Javascript, Angular, Express.js and Node.js Strong engineering professional with a Bachelor's degree focused in Electronics and Communications Engineering from Visvesvaraya Technological University. `
+    ],
+    currentProject: "Web3 and AI",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Principle Software Engineer",
+      company: "Springboard",
+      year: "2016-2022",
+      companyLink: "https://springboard.com",
+      desc: "Lead many projects",
+    },
+    {
+      title: "Lead Software Engineer",
+      company: "Bukuwarung",
+      year: "2021",
+      companyLink: "https://bukuwarung.com",
+      desc: "Built the KYC web view to onboard new merchants",
+    },
+    {
+      title: "Software Engineer",
+      company: "Hewlett Packard Enterprise",
+      year: "2021",
+      companyLink: "https://hpe.com",
+      desc: "Mobile Number Portability",
+    }
+  ],
+  resumeUrl:
+    "",
+  socialLinks: {
+    instagram: "https://instagram.com/",
+    twitter: "https://twitter.com/",
+    linkedin: "https://linkedin.com/in/",
+    github: "https://github.com/",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
