07b2cb1c3de492d5b2cd44990a66c79333fbdc6f
sachinc <sachin@milenzer.com>
2023-01-23 15:58:57 +0530

    fix styling and projects page

--- components/Projects.js
+++ components/Projects.js
@@ -4,14 +4,14 @@ import userData from "@constants/data";
 export default function Projects() {
   return (
     <section className="bg-white dark:bg-gray-800">
-      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
-        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+      <div className="max-w-6xl mx-auto h-20 bg-white dark:bg-gray-800 mb-12 md:mb-28">
+        <h1 className=" text-5xl md:text-9xl font-bold py-4 text-center md:text-left">
           Projects
         </h1>
       </div>
       {/* Grid starts here */}
       <div className="bg-[#F1F1F1] dark:bg-gray-900">
-        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-20 pb-40">
+        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 pb-4">
           {userData.projects.map((proj, idx) => (
             <ProjectCard
               title={proj.title}
@@ -30,7 +30,7 @@ const ProjectCard = ({ title, link, imgUrl, number }) => {
   return (
     <a href={link} className="w-full block shadow-2xl">
       <div className="relative overflow-hidden">
-        <div className="h-72 object-cover">
+        <div className="h-96 object-cover">
           <img
             src={imgUrl}
             alt="portfolio"
